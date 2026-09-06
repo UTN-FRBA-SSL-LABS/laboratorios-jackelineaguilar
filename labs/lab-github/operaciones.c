6258b43804dc5772ff1382a2f3fdedd4c9acff42
Jackeline Aguilar <jaguilar@frba.utn.edu.ar>
2026-09-06 02:57:01 +0000

    funcion multiplicar documentada

--- labs/lab-github/operaciones.c
+++ labs/lab-github/operaciones.c
@@ -14,6 +14,8 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
+/* Multiplica dos enteros utilizando el operador aritmetico *. */
+
 int multiplicar(int a, int b) {
     return a * b;
 }
